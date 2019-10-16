89cc1bc5e422b9035c9db972c7d54a3086ed941e
Unknown <Hector2.Martin-Davies@live.uwe.ac.uk>
2019-10-16 19:09:06 +0100

    Mouse Inputs

--- SimSomthing/hw3d/Window.h
+++ SimSomthing/hw3d/Window.h
@@ -21,6 +21,8 @@
 #include "ChiliWin.h"
 #include "ChiliException.h"
 #include "Keyboard.h"
+#include "Mouse.h"
+#include <optional>
 
 
 class Window
@@ -59,12 +61,15 @@ public:
 	~Window();
 	Window( const Window& ) = delete;
 	Window& operator=( const Window& ) = delete;
+	void SetTitle( const std::string& title );
+	static std::optional<int> ProcessMessages();
 private:
 	static LRESULT CALLBACK HandleMsgSetup( HWND hWnd,UINT msg,WPARAM wParam,LPARAM lParam ) noexcept;
 	static LRESULT CALLBACK HandleMsgThunk( HWND hWnd,UINT msg,WPARAM wParam,LPARAM lParam ) noexcept;
 	LRESULT HandleMsg( HWND hWnd,UINT msg,WPARAM wParam,LPARAM lParam ) noexcept;
 public:
 	Keyboard kbd;
+	Mouse mouse;
 private:
 	int width;
 	int height;
